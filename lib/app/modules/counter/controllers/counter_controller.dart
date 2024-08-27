9205d8aded88cb6676ebe902bb6d039b420d866a
riskasetiad <rskdeswita@gmail.com>
2024-08-27 11:43:25 +0700

    biodata dan output

--- lib/app/modules/counter/controllers/counter_controller.dart
+++ lib/app/modules/counter/controllers/counter_controller.dart
@@ -12,18 +12,21 @@ class CounterController extends GetxController {
       bilangan.value++;
     }
   }
+
   void kurangSatu() {
     if (bilangan.value <= 0) {
       Get.snackbar('Warning', 'stopppp',
-          snackPosition: SnackPosition.BOTTOM, backgroundColor: const Color.fromARGB(255, 255, 7, 7));
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: const Color.fromARGB(255, 255, 7, 7));
     } else {
       bilangan.value--;
     }
   }
+
   void refresh() {
-     Get.snackbar('Warning', 'jadian lagi ya',
+    Get.snackbar('Warning', 'jadian lagi ya',
         snackPosition: SnackPosition.BOTTOM,
         backgroundColor: Color.fromARGB(255, 48, 255, 7));
-      bilangan.value = 0;
+    bilangan.value = 0;
   }
 }
